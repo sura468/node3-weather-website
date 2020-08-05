8f56ad846ac29112f8dc8b50e2e6d27864078292
unknown <sura468@gmail.com>
2020-08-05 09:52:17 -0400

    Remove unnecessary console.log

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side Jave Script is loaded')
+
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageone = document.querySelector('#message-1')
